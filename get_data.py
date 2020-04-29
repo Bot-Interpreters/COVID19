eb7c22626655d375e1572d89397c8c75e7b5f463
Gautam J <gautam.jayapal@gmail.com>
2020-04-29 14:04:20 +0530

    Log statements

--- get_data.py
+++ get_data.py
@@ -8,10 +8,17 @@ data = requests.get(raw_data_url, allow_redirects=True)
 with open('data\\raw_data.csv', 'wb') as f:
     f.write(data.content)
 
+print('[INFO] Collected raw_data.csv')
+
 data = requests.get(states_daily_url, allow_redirects=True)
 with open('data\\states_daily.json', 'wb') as f:
     f.write(data.content)
 
+print('[INFO] Collected states_daily.json')
+
 data = requests.get(time_series_url, allow_redirects=True)
 with open('data\\time_series.json', 'wb') as f:
     f.write(data.content)
+
+print('[INFO] Collected time_series.json')
+input('Press ENTER to quit...')
