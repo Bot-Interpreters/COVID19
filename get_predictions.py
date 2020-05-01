4c92521d1320397cacff685472b8b0a04002d8ee
Gautam J <gautam.jayapal@gmail.com>
2020-05-01 11:33:46 +0530

    Added Rolling Average Predictions

--- get_predictions.py
+++ get_predictions.py
@@ -15,7 +15,18 @@ for col in confirmed_df:
     confirmed_df[col] = pd.to_numeric(confirmed_df[col])
 
 
-def get_number_of_confirmed_cases(series, title='', future_days=3):
+def get_predictions_rolling_average(series, title='', window=3):
+    prediction_date = series.index[-1] + datetime.timedelta(days=1)
+    total_cases_window = 0
+    for i in range(1, window + 1):
+        total_cases_window += series[-i]
+    prediction_value = total_cases_window / window
+
+    print(f'\nType of Prediction: {title}, Model: Rolling Average ({window} days)')
+    print(f"Date: {prediction_date.strftime('%Y-%m-%d')} Prediction: {round(prediction_value)}")
+
+
+def get_number_of_confirmed_cases_arima(series, title='', future_days=3):
 
     arima_model = pm.auto_arima(series,
                                 trace=False,
@@ -25,13 +36,13 @@ def get_number_of_confirmed_cases(series, title='', future_days=3):
     predictions = arima_model.predict(n_periods=future_days)
     dates = [series.index[-1] + datetime.timedelta(days=i) for i in range(1, future_days + 1)]
 
-    print(f'\nType of Prediction: {title}')
+    print(f'\nType of Prediction: {title}, Model: ARIMA')
 
     for i in range(future_days):
         print(f"Date: {dates[i].strftime('%Y-%m-%d')} Prediction: {round(predictions[i])}")
 
 
-def get_predictions(place='tn'):
+def get_predictions_arima_and_rolling_average(place='tn'):
     df = confirmed_df[place]
     df_cum = confirmed_df[place].cumsum()
 
@@ -42,12 +53,14 @@ def get_predictions(place='tn'):
     else:
         title = place.upper()
 
-    get_number_of_confirmed_cases(df, title=f'{title} Confirmed Cases')
-    get_number_of_confirmed_cases(df_cum, title=f'{title} Confirmed Cases Cumulative')
+    get_number_of_confirmed_cases_arima(df, title=f'{title} Confirmed Cases')
+    get_predictions_rolling_average(df, title=f'{title} Confirmed Cases')
+    get_number_of_confirmed_cases_arima(df_cum, title=f'{title} Confirmed Cases Cumulative')
+    get_predictions_rolling_average(df_cum, title=f'{title} Confirmed Cases Cumulative')
 
 
-get_predictions('tt')
-get_predictions('tn')
+get_predictions_arima_and_rolling_average('tt')
+get_predictions_arima_and_rolling_average('tn')
 
 while True:
     ui = input("\nEnter state code abbreviation, 'q' to quit: ")
@@ -55,4 +68,4 @@ while True:
     if ui == 'q':
         break
 
-    get_predictions(ui)
+    get_predictions_arima_and_rolling_average(ui)
